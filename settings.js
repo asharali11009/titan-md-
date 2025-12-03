939e8cbe7de1f127f19929d9b8f2a37d79890a5e
asharali11009 <noahext994+@gmail.com>
2025-12-03 06:28:40 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'ezio',
   botName: "TITAN",
   botOwner: 'ezio', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '923164413714', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
